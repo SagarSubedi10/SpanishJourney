88f19aac5ee6bb2d3b1b4e0dd28b738750fbe455
sagarsubedi10 <s.subedisagar@gmail.com>
2023-07-27 20:04:09 -0500

    view count

---
+++ main.js
@@ -0,0 +1,17 @@
+// Get the element where you want to display the view count
+const viewCountElement = document.getElementById('viewCount');
+
+// Make a GET request to your API endpoint
+fetch('https://laz9kkqvca.execute-api.us-east-1.amazonaws.com/views')
+  .then(response => response.json())
+  .then(data => {
+    // Extract the view count from the response
+    const viewCount = data.views;
+
+    // Update the HTML content with the view count
+    viewCountElement.textContent = viewCount + ' Views';
+  })
+  .catch(error => {
+    console.error('Error:', error);
+ });
+  
